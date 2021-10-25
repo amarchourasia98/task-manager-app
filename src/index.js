0f6a53442db7dec47b9b2372c595705ce3ce36fc
Gaurav453 <51244581+Gaurav453@users.noreply.github.com>
2021-10-25 22:27:02 +0530

    Update index.js
    port fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const userouter = require('./routers/user')
 const taskrouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json())
